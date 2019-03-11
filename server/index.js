f934a00194363e2b2742e10bcf41ad41decc9472
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-03-11 22:56:13 +0700

    create API

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const apiRoute = require("./routes/api.route");
+
 const app = express();
 
 const port = process.env.PORT || 8080;
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Chao Xin"));
+app.get("/", (req, res) => res.send('<h2 style="color: green">Chao Xin!</h2>'));
+app.get("/error", (req, res) => res.send('<h2 style="color: red">Error</h2>'));
+app.use("/api", apiRoute);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
